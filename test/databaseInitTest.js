5c809d54d0fcbacd8643f33e266cab1ad8855e50
Henry <henry@wangqiru.com>
2018-03-18 19:23:54 +0000

    Add dbInit unit test

---
+++ test/databaseInitTest.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const expect = require('chai').expect;
+const db = require('../models');
+const siteRules = require('../config/siteRules');
+
+describe('Database init()', () => {
+  before((done) => {
+    db.sequelize.sync()
+      .then(async () => {
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+
+  it('SiteRules should have all pre-defined rules', async () => {
+    const siteRule = await db.SiteRule.findAll();
+    expect(siteRule.length).to.equal(siteRules.rules.length);
+  });
+
+
+  it('Users should have Henry', async () => {
+    const user = await db.User.findById(1);
+    expect(user.apiKey).to.equal(process.env.KEY);
+  });
+});
