bedf8a07b4e8dc6fceb68444d4fc0956ccf40ffd
Henry <henry@wangqiru.com>
2018-04-17 22:30:23 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker build -t wangqiru/api . && sudo docker rm api -f && sudo docker-compose up -d
+sudo docker build -t wangqiru/api . && (sudo docker stop api || true && sudo docker rm api || true)&& sudo docker-compose up -d
