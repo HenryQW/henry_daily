a4c93ba1ccab26b35567be42b21d6f8310941614
Henry <hi@henry.wang>
2018-08-23 02:41:49 +0100

    sequelize doesn't like to be tested

--- test/databaseInitTest.js
+++ test/databaseInitTest.js
@@ -5,26 +5,20 @@ const {
 const db = require('../models');
 const siteRules = require('../helpers/siteRules');
 
+before(async () => {
+  await db.sequelize.drop();
+  await db.sequelize.sync();
+  await db.User.create({ apiKey: process.env.KEY });
+});
 
 describe('database init', () => {
-  before(async () => {
-    await db.sequelize.sync();
-    await db.User.create({
-      apiKey: process.env.KEY,
-    });
-  });
-
   it('SiteRules should have all pre-defined rules', async () => {
     const siteRule = await db.SiteRule.findAll();
     expect(siteRule.length).to.equal(siteRules.rules.length);
   });
 
-  it('Users should have Henry', async () => {
+  it('Users should have api key', async () => {
     const user = await db.User.findById(1);
     expect(user.apiKey).to.equal(process.env.KEY);
-  });
-
-  after(async () => {
-    if (process.env.KEY !== 'henry_daily') { await db.sequelize.drop(); }
   });
 });
