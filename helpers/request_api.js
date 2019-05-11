f442abc4e177a961168500462d698310a25133bb
Henry <hi@henry.wang>
2019-05-11 22:37:11 +0100

    fix: request option scope

--- helpers/request_api.js
+++ helpers/request_api.js
@@ -1,20 +1,19 @@
 const rq = require('request-promise');
 
-const options = {
-    method: 'POST',
-    headers: {
-        'User-Agent': process.env.WECHAT_UA,
-        'Content-Type': 'application/json',
-    },
-    body: {
-        h_model: process.env.WECHAT_HEADER_MODEL,
-        h_ch: process.env.WECHAT_HEADER_CH,
-    },
-    json: true,
-};
-
 const request = async (uri, params) => {
-    options.uri = uri;
+    const options = {
+        method: 'POST',
+        uri,
+        headers: {
+            'User-Agent': process.env.WECHAT_UA,
+            'Content-Type': 'application/json',
+        },
+        body: {
+            h_model: process.env.WECHAT_HEADER_MODEL,
+            h_ch: process.env.WECHAT_HEADER_CH,
+        },
+        json: true,
+    };
 
     Object.keys(params).forEach((param) => {
         options.body[param] = params[param];
